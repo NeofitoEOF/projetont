41653aa4253ec2e17781c6a322eb54408cfafa9a
José Paulo <lorenconi16@gmail.com>
2022-01-30 00:58:16 -0300

    feat: Add validacao se usaurio esta auth

--- projnt/pages/welcomePage.js
+++ projnt/pages/welcomePage.js
@@ -1,36 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
- function welcomePage() {
+function welcomePage() {
+  const router = useRouter();
+  const [token, setToken] = useState("");
+  const [users, setUsers] = useState([]);
 
-  const ISSERVER = typeof window === "undefined"
-  if (!ISSERVER) {
-
-    const token = localStorage.getItem("token");
-    console.log("Aqui", token);
-    if (!token) {
-      return false;
-    }
-   
-    try {
-      const response = fetch("http://localhost:3333/users", {
+  useEffect(() => {
+    const ISSERVER = typeof window === "undefined";
+    if (!ISSERVER) {
+      const token = localStorage.getItem("token");
+      if (!!token) {
+        router.push("/");
+        return false;
+      }
+      setToken(token);
+      fetch("http://localhost:3333/users", {
         method: "GET",
         headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`
-        }
-
-      }).then(response.json())
-  console.log(response)
-    } catch {
-
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((user) => {
+          console.log(user.consultTask);
+          setUsers(user.infoUser.map((el) => el.username));
+        });
     }
-
-    return (
-
-
-      <div>Welcome Page</div>
-
-    )
-  }
+  }, []);
+  return (
+    <>
+      <h1>Welcome {users}</h1>
+      <h3>Task{users.consultTask}</h3>
+    </>
+  );
 }
-  export default welcomePage
+export default welcomePage;
